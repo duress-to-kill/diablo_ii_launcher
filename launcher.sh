47c460a95dff1b9679f905adc6934763c78ae554
Andrew J Wood <andywood@gentoo.cs.pdx.edu>
2014-04-16 19:45:53 -0700

    Copied sections of nightfly's launcher

--- launcher.sh
+++ launcher.sh
@@ -2,3 +2,16 @@
 [ -d ~/.d2_save ] || mkdir ~/.d2_save
 ln -s ~/.d2_save Save
 wine ./Diablo\ II.exe
+
+
+#source_folder="/stash/acm/starcraft/starcraft"
+#target_folder="/tmp/starcraft-`date +%s`"
+#server="iss.cat.pdx.edu";
+#channel="#starcraft";
+#port="6667";
+
+#mkdir $target_folder
+#cd $target_folder
+#find $source_folder -mindepth 1 -maxdepth 1 | xargs -I ITEM bash -c 'ln -s ITEM `basename ITEM`'
+#rm maps
+#mkdir maps
